1228b0b67179959f2ceabe96278e93273927ec72
2eo22 <2eo2yeo@gmail.com>
2024-12-27 11:27:34 +0900

    11

---
+++ my_sql/mysql84_실습코드.sql
@@ -0,0 +1,14 @@
+
+/* 
+	SQL(Structrured Query Language) : 데이터 베이스에서 사용하는 구조화된 언어
+ */ 
+
+
+show databases;
+use hrdb2019;
+select database();
+show tables;
+select * from employee;
+select * from department;
+select * from unit;
+select * from vacation;
